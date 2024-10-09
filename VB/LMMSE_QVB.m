b0e9e06f229acc9db32eb9eaddafe195ef15d8be
SDSU-WiiLAB <duy.nguyen@sdsu.edu>
2024-10-09 10:52:39 -0700

    Update LMMSE_QVB.m

--- VB/LMMSE_QVB.m
+++ VB/LMMSE_QVB.m
@@ -1,3 +1,11 @@
+% Stable version of LMMSE-VB for data detection with few-bit observations
+% @ Developed by Duy Nguyen, duy.nguyen@sdsu.com
+% Please cite the following paper if you use the code
+% 1) Duy H. N. Nguyen, Italo Atzeni, Antti Tölli, A Lee Swindlehurst, "A Variational Bayesian Perspective on Massive MIMO Detection," 
+% arXiv preprint arXiv:2205.11649. https://arxiv.org/pdf/2205.11649
+% 2) Ly V. Nguyen, A Lee Swindlehurst, Duy H. N. Nguyen, "Variational Bayes for joint channel estimation and data detection in few-bit massive MIMO systems,"
+% IEEE Transactions on Signal Processing, July 2024. DOI: 10.1109/TSP.2024.3429009
+
 function [x_est, q, r, sigma_r] = LMMSE_QVB(par, H, y, y_l, y_u, delta)
 K = par.K;
 M = par.M;
